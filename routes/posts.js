af7355db65bac8afc6d4848278fd3cf43aec0542
Ramanpreet Kaur <ramanpreetkaurr04@gmail.com>
2025-07-23 10:46:14 +0530

    auto update

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Post = require('./models/Post');
+const Post = require('../models/Post');
 const auth = require('../middleware/auth');
 
 // Create Post
